c26557a952e28ad119c7180b8464a86056ea0cfb
ammarinwp22770-pixel <ammarinwp22770@gmail.com>
2025-10-14 11:38:14 +0700

    Use dynamic PORT for Render

--- server.js
+++ server.js
@@ -267,4 +267,5 @@ app.get("/alert", (req, res) => {
 });
 
   // ✅ เริ่มรันเซิร์ฟเวอร์
-  app.listen(3000, () => console.log("✅ Server running at http://localhost:3000"));
+ const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
